e79a2683932d5b52e141b91c7573182878142a98
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2015-07-09 16:02:09 +0200

    initial commit

---
+++ modules/recognition/include/v4r/recognition/ghv_cuda.h
@@ -0,0 +1,300 @@
+#ifndef FAAT_PCL_GHV_CUDA_H_
+#define FAAT_PCL_GHV_CUDA_H_
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/copy.h>
+#include <thrust/device_ptr.h>
+#include <thrust/sequence.h>
+#include <stdio.h>
+//#include <pcl/point_types.h>
+#include <pcl/gpu/containers/device_array.h>
+//#include <pcl/gpu/octree/src/internal.hpp>
+#include <pcl/gpu/octree/octree.hpp>
+#include <pcl/gpu/octree/device_format.hpp>
+
+#define cudaCheckErrors(msg) \
+    do { \
+        cudaError_t __err = cudaGetLastError(); \
+        if (__err != cudaSuccess) { \
+            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
+                msg, cudaGetErrorString(__err), \
+                __FILE__, __LINE__); \
+            fprintf(stderr, "*** FAILED - ABORTING\n"); \
+            exit(1); \
+        } \
+    } while (0)
+
+namespace faat_pcl
+{
+
+namespace recognition_cuda
+{
+
+struct xyz_p
+{
+    float x;
+    float y;
+    float z;
+};
+
+struct pointExtended
+{
+    xyz_p p;
+    int hypotheses_id;
+    int model_id;
+    int point_id;
+    bool set;
+    float3 color; //color space independent
+    bool color_set_;
+    int hypotheses_type_; //0 for objects, 1 for planes
+};
+
+struct explainedSPExtended
+{
+    int hypotheses_id;
+    int scene_idx;
+};
+
+struct rgb
+{
+    float r;
+    float g;
+    float b;
+};
+
+struct Mat4f {
+    float mat[4][4];
+};
+
+struct HypothesisGPU
+{
+  Mat4f transform_;
+  int model_idx_;
+  int size_;
+  /*bool * p_visible_;
+  int * visible_;*/
+};
+
+class XYZPointCloud
+{
+    public:
+
+        __host__ __device__
+        XYZPointCloud()
+        {
+
+        }
+
+        __host__
+        ~XYZPointCloud()
+        {
+            if(on_device_)
+            {
+                if(points)
+                {
+                    cudaFree(points);
+                    cudaCheckErrors("cudaFree fail");
+                }
+            }
+            else
+            {
+                if(points)
+                    delete[] points;
+            }
+        }
+
+        int width_;
+        int height_;
+        bool on_device_;
+        xyz_p * points;
+
+        __device__ __host__
+        xyz_p at(int u, int v)
+        {
+            return points[u * width_ + v];
+        }
+};
+
+//scene should be allocated in GPU memory
+//points of the model should also be allocated in GPU
+
+class GHV
+{
+    pcl::gpu::DeviceArray2D<xyz_p> scene_;
+    pcl::gpu::DeviceArray<xyz_p> scene_downsampled_;
+    pcl::gpu::DeviceArray<float3> scene_rgb_values_;
+    XYZPointCloud * scene_downsampled_host_;
+    XYZPointCloud * scene_normals_host_;
+
+    pcl::gpu::DeviceArray<xyz_p> * model_clouds_device_array_;
+    pcl::gpu::DeviceArray<xyz_p> * model_normals_device_array_;
+    pcl::gpu::DeviceArray<float3> * model_colours_device_array_;
+
+    xyz_p ** model_clouds_;
+    xyz_p ** model_normals_;
+    float3 ** model_rgb_values_;
+
+    std::vector<HypothesisGPU> hypotheses_;
+    pcl::gpu::DeviceArray<HypothesisGPU> hypotheses_dev_;
+    size_t n_models_;
+    std::vector<XYZPointCloud *> model_clouds_host_;
+    int total_points_hypotheses_;
+
+    std::vector<int> labels_; //scene_downsampled_.size()
+    pcl::gpu::Octree octree_device_;
+
+    //parameters
+    float inlier_threshold;
+    float clutter_regularizer_;
+    float outlier_regularizer_;
+    bool detect_clutter_;
+    float clutter_radius_;
+    float color_sigma_y_, color_sigma_ab_;
+
+    //output variables
+    pcl::gpu::DeviceArray<float> angles_with_vp_;
+    std::vector< std::vector<int> > visible_points_; //point indices that are visible for each hypothesis
+    pcl::gpu::DeviceArray<pointExtended> extended_points;
+    pcl::gpu::DeviceArray<pointExtended> visible_extended_points;
+    thrust::device_vector<int> visible_sizes_per_hypothesis_; //hypotheses_.size() vector
+    thrust::device_vector<float> outliers_per_hypothesis_; //hypotheses_.size() vector
+    pcl::gpu::DeviceArray<pointExtended> outlier_extended_points_;
+
+    thrust::device_vector<int> getHypothesesIdsFromPointExtended(thrust::device_vector<int> & indices,
+                                                                 pcl::gpu::DeviceArray<pointExtended> & points);
+
+    thrust::device_vector<int> getHypothesesIdsFromPointExtendedWithIndices(thrust::device_vector<int> & indices,
+                                                                 pcl::gpu::DeviceArray<pointExtended> & points);
+
+
+    std::vector< std::vector<int> > explained_points_;
+    std::vector< std::vector<float> > explained_points_weights_;
+    std::vector< std::vector<int> > unexplained_points_;
+    std::vector< std::vector<float> > unexplained_points_weights_;
+
+    std::vector<int> solution_global_;
+    bool color_exists_;
+public:
+    GHV()
+    {
+        outlier_regularizer_ = 2.f;
+        inlier_threshold = 0.008f;
+        clutter_regularizer_ = 5.f;
+        detect_clutter_ = true;
+        color_exists_ = false;
+        clutter_radius_ = 0.03f;
+        color_sigma_y_ = color_sigma_ab_ = 0.5f;
+    }
+
+    void setclutterRadius(float f)
+    {
+        clutter_radius_ = f;
+    }
+
+    void setInlierThreshold(float i)
+    {
+        inlier_threshold = i;
+    }
+
+    void setOutlierWewight(float i)
+    {
+        outlier_regularizer_ = i;
+    }
+
+    void setClutterWeight(float i)
+    {
+        clutter_regularizer_ = i;
+    }
+
+    void setColorSigmas(float cs_y, float cs_ab)
+    {
+        color_sigma_y_ = cs_y;
+        color_sigma_ab_ = cs_ab;
+    }
+
+    ~GHV()
+    {
+
+    }
+
+    void setDetectClutter(bool b)
+    {
+        detect_clutter_ = b;
+    }
+
+    void setSmoothLabels(std::vector<int> & labels)
+    {
+        labels_ = labels;
+    }
+
+    void freeMemory();
+
+    //scene cloud used for visibility checking (organized)
+    void setSceneCloud(XYZPointCloud * cloud);
+
+    void setSceneRGBValues(float3 * rgb_scene_values, int size);
+
+    //unorganized cloud used to compute cues and optimize
+    void setScenePointCloud(XYZPointCloud * cloud);
+
+    void setModelClouds(std::vector<XYZPointCloud *> & model_clouds,
+                        std::vector<XYZPointCloud *> & model_normals);
+
+    void setModelColors(std::vector<float3 *> & model_colors, int models_size, std::vector<int> & sizes_per_model);
+
+    void setSceneNormals(XYZPointCloud * normals);
+
+    void setHypotheses(std::vector<HypothesisGPU> & hypotheses);
+
+    void computeExplainedAndModelCues();
+
+    void computeClutterCue();
+
+    void computeVisibility();
+
+    void getVisible(std::vector< std::vector<int> > & visible_points)
+    {
+        visible_points = visible_points_;
+    }
+
+    void getVisibleAndOutlierSizes(std::vector< std::pair<int, int> > & visible_and_outlier_sizes)
+    {
+        for(size_t i=0; i < hypotheses_.size(); i++)
+        {
+            std::pair<int, int> p = std::make_pair(visible_sizes_per_hypothesis_[i], outliers_per_hypothesis_[i]);
+            visible_and_outlier_sizes.push_back(p);
+        }
+    }
+
+    void getExplainedPointsAndWeights(std::vector< std::vector<int> > & explained,
+                                      std::vector< std::vector<float> > & explained_weights)
+    {
+        explained = explained_points_;
+        explained_weights = explained_points_weights_;
+    }
+
+    void getUnexplainedPointsAndWeights(std::vector< std::vector<int> > & unexplained,
+                                        std::vector< std::vector<float> > & unexplained_weights)
+    {
+        unexplained = unexplained_points_;
+        unexplained_weights = unexplained_points_weights_;
+    }
+
+    void optimize();
+
+    void getSolution(std::vector<bool> & solution)
+    {
+        solution.resize(solution_global_.size(), false);
+        for(size_t i=0; i < solution_global_.size(); i++)
+        {
+            if(solution_global_[i] == 1)
+                solution[i] = true;
+        }
+    }
+
+};
+}
+}
+
+#endif //FAAT_PCL_GHV_CUDA_H_
---
+++ modules/recognition/include/v4r/recognition/model_only_source.h
@@ -0,0 +1,182 @@
+/*
+ * ply_source.h
+ *
+ *  Created on: Mar 9, 2012
+ *      Author: aitor
+ */
+
+#ifndef FAAT_PCL_REC_FRAMEWORK_MODEL_VIEWS_SOURCE_H_
+#define FAAT_PCL_REC_FRAMEWORK_MODEL_VIEWS_SOURCE_H_
+
+#include "source.h"
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include "v4r/ORUtils/faat_3d_rec_framework_defines.h"
+#include "vtk_model_sampling.h"
+#include <vtkTransformPolyDataFilter.h>
+#include <pcl/segmentation/supervoxel_clustering.h>
+#include <v4r/utils/filesystem_utils.h>
+
+namespace faat_pcl
+{
+  namespace rec_3d_framework
+  {
+
+    /**
+     * \brief Data source class based on partial views from sensor.
+     * In this case, the training data is obtained directly from a depth sensor.
+     * The filesystem should contain pcd files (representing a view of an object in
+     * camera coordinates) and each view needs to be associated with a txt file
+     * containing a 4x4 matrix representing the transformation from camera coordinates
+     * to a global object coordinates frame.
+     * \author Aitor Aldoma
+     */
+
+    template<typename Full3DPointT = pcl::PointXYZRGBNormal, typename PointInT = pcl::PointXYZRGB>
+      class ModelOnlySource : public Source<PointInT>
+      {
+        typedef Source<PointInT> SourceT;
+        typedef Model<PointInT> ModelT;
+        typedef boost::shared_ptr<ModelT> ModelTPtr;
+
+        using SourceT::path_;
+        using SourceT::models_;
+        using SourceT::model_scale_;
+        using SourceT::load_into_memory_;
+        using SourceT::radius_normals_;
+        using SourceT::compute_normals_;
+        std::string ext_;
+
+        void computeFaces(ModelT & model);
+
+      public:
+        ModelOnlySource ()
+        {
+          load_into_memory_ = false;
+          ext_ = "pcd";
+        }
+
+        void setExtension(std::string e)
+        {
+            ext_ = e;
+        }
+
+        void
+        loadOrGenerate (std::string & dir, std::string & model_path, ModelT & model)
+        {
+          if(ext_.compare("pcd") == 0)
+          {
+              std::stringstream full_model;
+              full_model << path_ << "/" << "/" << model.class_ << "/" << model.id_;
+              typename pcl::PointCloud<Full3DPointT>::Ptr modell (new pcl::PointCloud<Full3DPointT>);
+              typename pcl::PointCloud<Full3DPointT>::Ptr modell_voxelized (new pcl::PointCloud<Full3DPointT>);
+              pcl::io::loadPCDFile(full_model.str(), *modell);
+
+              float voxel_grid_size = 0.001f;
+              typename pcl::VoxelGrid<Full3DPointT> grid_;
+              grid_.setInputCloud (modell);
+              grid_.setLeafSize (voxel_grid_size, voxel_grid_size, voxel_grid_size);
+              grid_.setDownsampleAllData (true);
+              grid_.filter (*modell_voxelized);
+
+              model.normals_assembled_.reset(new pcl::PointCloud<pcl::Normal>);
+              model.assembled_.reset (new pcl::PointCloud<PointInT>);
+
+              pcl::copyPointCloud(*modell_voxelized, *model.assembled_);
+              pcl::copyPointCloud(*modell_voxelized, *model.normals_assembled_);
+
+              for(size_t kk=0; kk < model.normals_assembled_->points.size(); kk++)
+              {
+                  Eigen::Vector3f normal = model.normals_assembled_->points[kk].getNormalVector3fMap();
+                  normal.normalize();
+                  model.normals_assembled_->points[kk].getNormalVector3fMap() = normal;
+              }
+
+              computeFaces(model);
+
+          }
+          else if(ext_.compare("ply") == 0)
+          {
+
+              typename pcl::PointCloud<PointInT>::Ptr model_cloud(new pcl::PointCloud<PointInT>());
+              uniform_sampling (model_path, 100000, *model_cloud, model_scale_);
+
+              float resolution = 0.001f;
+              pcl::VoxelGrid<PointInT> grid_;
+              grid_.setInputCloud (model_cloud);
+              grid_.setLeafSize (resolution, resolution, resolution);
+              grid_.setDownsampleAllData(true);
+
+              model.assembled_.reset (new pcl::PointCloud<PointInT>);
+              grid_.filter (*(model.assembled_));
+
+              if(compute_normals_)
+              {
+                std::cout << "Computing normals for ply models... " << radius_normals_ << std::endl;
+                model.computeNormalsAssembledCloud(radius_normals_);
+                model.setFlipNormalsBasedOnVP(true);
+              }
+          }
+        }
+        /**
+         * \brief Creates the model representation of the training set, generating views if needed
+         */
+        void
+        generate (std::string & training_dir)
+        {
+          //get models in directory
+          std::vector < std::string > files;
+
+          v4r::utils::getFilesInDirectory (path_, files, "", ".*.pcd", false);
+          std::cout << files.size() << std::endl;
+
+          models_.reset (new std::vector<ModelTPtr>);
+
+          for (size_t i = 0; i < files.size (); i++)
+          {
+            ModelTPtr m(new ModelT());
+
+            std::vector < std::string > strs;
+            boost::split (strs, files[i], boost::is_any_of ("/\\"));
+            std::string name = strs[strs.size () - 1];
+
+            if (strs.size () == 1)
+            {
+              m->id_ = strs[0];
+            }
+            else
+            {
+              std::stringstream ss;
+              for (int i = 0; i < (static_cast<int> (strs.size ()) - 1); i++)
+              {
+                ss << strs[i];
+                if (i != (static_cast<int> (strs.size ()) - 1))
+                  ss << "/";
+              }
+
+              m->class_ = ss.str ();
+              m->id_ = strs[strs.size () - 1];
+            }
+
+            std::cout << m->class_ << " . " << m->id_ << std::endl;
+            //check which of them have been trained using training_dir and the model_id_
+            //load views, poses and self-occlusions for those that exist
+            //generate otherwise
+
+            std::stringstream model_path;
+            model_path << path_ << "/" << files[i];
+            std::string path_model = model_path.str ();
+            loadOrGenerate (training_dir, path_model, *m);
+
+            models_->push_back (m);
+
+            //std::cout << files[i] << std::endl;
+          }
+        }
+      };
+  }
+}
+
+#endif /* REC_FRAMEWORK_MESH_SOURCE_H_ */
