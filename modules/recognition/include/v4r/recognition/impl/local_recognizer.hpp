07b222782792581cb643c1c37bf93a36f43f10f6
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2016-04-19 17:26:45 +0100

    use multi-plane segmentation for local recognizer to find *heighest* table plane

--- modules/recognition/include/v4r/recognition/impl/local_recognizer.hpp
+++ modules/recognition/include/v4r/recognition/impl/local_recognizer.hpp
@@ -5,6 +5,7 @@
 #include <v4r/features/types.h>
 #include <v4r/io/eigen.h>
 #include <v4r/segmentation/dominant_plane_segmenter.h>
+#include <v4r/segmentation/multiplane_segmenter.h>
 #include <v4r/recognition/local_recognizer.h>
 
 #include <pcl/features/boundary.h>
@@ -586,13 +587,17 @@ LocalRecognitionPipeline<PointT>::computeFeatures()
 //        table_plane.normalize();
 
 
-        typename DominantPlaneSegmenter<PointT>::Parameter dp_seg_param;
-        dp_seg_param.compute_table_plane_only_ = true;
-        DominantPlaneSegmenter<PointT> dp_seg ( dp_seg_param );
-        dp_seg.setInputCloud(scene_);
-        dp_seg.setNormalsCloud(scene_normals_);
-        dp_seg.segment();
-        Eigen::Vector4f table_plane = dp_seg.getTablePlane();
+//        typename DominantPlaneSegmenter<PointT>::Parameter seg_param;
+//        seg_param.compute_table_plane_only_ = true;
+//        DominantPlaneSegmenter<PointT> seg ( seg_param );
+        typename MultiplaneSegmenter<PointT>::Parameter seg_param;
+        seg_param.min_cluster_size_ = 10000;
+        seg_param.sensor_noise_max_ = 0.015f;
+        MultiplaneSegmenter<PointT> seg (seg_param);
+        seg.setInputCloud(scene_);
+        seg.setNormalsCloud(scene_normals_);
+        seg.segment();
+        Eigen::Vector4f table_plane = seg.getTablePlane();
 
         if (0)//inliers->indices.empty())
         {
@@ -601,6 +606,8 @@ LocalRecognitionPipeline<PointT>::computeFeatures()
         else
         {
             typename pcl::PointCloud<PointT>::Ptr filtered_scene (new pcl::PointCloud<PointT>(*scene_));
+//            typename pcl::PointCloud<PointT>::Ptr filtered_scene2 (new pcl::PointCloud<PointT>(*scene_));
+//            typename pcl::PointCloud<PointT>::Ptr table_plane_pcl (new pcl::PointCloud<PointT>(*scene_));
             for (size_t j = 0; j < filtered_scene->points.size (); j++)
             {
                 const Eigen::Vector4f xyz_p = filtered_scene->points[j].getVector4fMap ();
@@ -616,7 +623,31 @@ LocalRecognitionPipeline<PointT>::computeFeatures()
                     filtered_scene->points[j].y = std::numeric_limits<float>::quiet_NaN ();
                     filtered_scene->points[j].z = std::numeric_limits<float>::quiet_NaN ();
                 }
+//                                if (val > 0.01f)
+//                                {
+//                                    filtered_scene2->points[j].x = std::numeric_limits<float>::quiet_NaN ();
+//                                    filtered_scene2->points[j].y = std::numeric_limits<float>::quiet_NaN ();
+//                                    filtered_scene2->points[j].z = std::numeric_limits<float>::quiet_NaN ();
+//                                }
+
+//                                if (std::abs(val) < 0.01f)
+//                                {
+//                                   table_plane_pcl->points[j].r = 255.f;
+//                                    table_plane_pcl->points[j].g = table_plane_pcl->points[j].b = 0.f;
+//                               }
             }
+
+//                        pcl::visualization::PCLVisualizer vis;
+//                       int vp1,vp2,vp3;
+//                       vis.createViewPort(0,0,0.33,1,vp1);
+//                        vis.createViewPort(0.33,0,0.66,1,vp2);
+//                        vis.createViewPort(0.66,0,1,1,vp3);
+//                        vis.addPointCloud(filtered_scene, "pos", vp1);
+//                        vis.addPointCloud(filtered_scene2, "neg", vp2);
+//                        vis.addPointCloud(table_plane_pcl, "tableplane", vp3);
+//                        vis.resetCamera();
+//                        vis.spin();
+//                        vis.removeAllPointClouds();
 
             if(filtered_scene->points.size() > 1000)
                 scene_ = filtered_scene;
