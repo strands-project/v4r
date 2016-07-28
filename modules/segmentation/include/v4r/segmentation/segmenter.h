a14c9ef5529279ba7fc0d56a2b5e984e69cc9280
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2016-07-28 15:06:45 +0200

    put some header defintions into cpp files and remove .hpp files

--- modules/segmentation/include/v4r/segmentation/segmenter.h
+++ modules/segmentation/include/v4r/segmentation/segmenter.h
@@ -60,7 +60,10 @@ protected:
     bool visualize_;
 
 public:
-    virtual ~Segmenter() = 0;
+    typedef boost::shared_ptr< Segmenter<PointT> > Ptr;
+    typedef boost::shared_ptr< Segmenter<PointT> const> ConstPtr;
+
+    virtual ~Segmenter(){}
 
     /**
      * @brief sets the cloud which ought to be segmented
@@ -130,89 +133,13 @@ public:
      * @brief visualize found clusters
      */
     void
-    visualize() const
-    {
-        if(!vis_)
-        {
-            vis_.reset ( new pcl::visualization::PCLVisualizer("Segmentation Results") );
-            vis_->createViewPort(0,0,0.33,1,vp1_);
-            vis_->createViewPort(0.33,0,0.66,1,vp2_);
-            vis_->createViewPort(0.66,0,1,1,vp3_);
-        }
-        vis_->removeAllPointClouds();
-        vis_->removeAllShapes();
-        vis_->addPointCloud(scene_, "cloud", vp1_);
+    visualize() const;
 
-
-        typename pcl::PointCloud<PointT>::Ptr colored_cloud (new pcl::PointCloud<PointT>());
-        for(size_t i=0; i < clusters_.size(); i++)
-        {
-            pcl::PointCloud<PointT> cluster;
-            pcl::copyPointCloud(*scene_, clusters_[i], cluster);
-
-            const uint8_t r = rand()%255;
-            const uint8_t g = rand()%255;
-            const uint8_t b = rand()%255;
-            for(size_t pt_id=0; pt_id<cluster.points.size(); pt_id++)
-            {
-                cluster.points[pt_id].r = r;
-                cluster.points[pt_id].g = g;
-                cluster.points[pt_id].b = b;
-            }
-            *colored_cloud += cluster;
-        }
-        vis_->addPointCloud(colored_cloud,"segments", vp2_);
-
-        typename pcl::PointCloud<PointT>::Ptr planes (new pcl::PointCloud<PointT>(*scene_));
-
-        Eigen::Matrix3Xf plane_colors(3, all_planes_.size());
-        for(size_t i=0; i<all_planes_.size(); i++)
-        {
-            plane_colors(0, i) = rand()%255;
-            plane_colors(1, i) = rand()%255;
-            plane_colors(2, i) = rand()%255;
-        }
-
-        for(PointT &pt :planes->points)
-        {
-            if ( !pcl::isFinite( pt ) )
-                continue;
-
-            const Eigen::Vector4f xyz_p = pt.getVector4fMap ();
-            pt.g = pt.b = pt.r = 0;
-
-
-            for(size_t i=0; i<all_planes_.size(); i++)
-            {
-                float val = xyz_p.dot(all_planes_[i]->coefficients_);
-
-                if ( std::abs(val) < 0.02f)
-                {
-                    pt.r = plane_colors(0,i);
-                    pt.g = plane_colors(1,i);
-                    pt.b = plane_colors(2,i);
-                }
-            }
-
-            float val = xyz_p.dot(dominant_plane_);
-
-            if ( std::abs(val) < 0.02f)
-                pt.r = 255;
-        }
-        vis_->addPointCloud(planes,"table plane", vp3_);
-        vis_->addText("input", 10, 10, 15, 1, 1, 1, "input", vp1_);
-        vis_->addText("segments", 10, 10, 15, 1, 1, 1, "segments", vp2_);
-        vis_->addText("dominant plane", 10, 10, 15, 1, 1, 1, "dominant_plane", vp3_);
-        vis_->addText("all other planes", 10, 25, 15, 1, 1, 1, "other_planes", vp3_);
-        vis_->resetCamera();
-        vis_->spin();
-    }
-
+    /**
+     * @brief segment
+     */
     virtual void
     segment() = 0;
-
-    typedef boost::shared_ptr< Segmenter<PointT> > Ptr;
-    typedef boost::shared_ptr< Segmenter<PointT> const> ConstPtr;
 };
 
 }
--- modules/segmentation/src/euclidean_segmenter.cpp
+++ modules/segmentation/src/euclidean_segmenter.cpp
@@ -2,5 +2,110 @@
 
 namespace v4r
 {
+
+template <typename PointT>
+void
+EuclideanSegmenter<PointT>::computeTablePlanes()
+{
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (param_.sensor_noise_max_);
+
+    while(true)
+    {
+        pcl::copyPointCloud(*scene_xyz_, *cloud_filtered);
+        for(size_t i=0; i<filter_mask_.size(); i++)
+        {
+            if( !filter_mask_[i] )
+            {
+                pcl::PointXYZ &p = cloud_filtered->points[i];
+                p.x = p.y = p.z = std::numeric_limits<float>::quiet_NaN();
+            }
+        }
+
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+
+        if ( (int)inliers->indices.size() < param_.num_plane_inliers_ )
+            break;
+
+        typename PlaneModel<PointT>::Ptr pm (new PlaneModel<PointT>);
+        pm->coefficients_ = Eigen::Vector4f(coefficients->values[0], coefficients->values[1],
+                coefficients->values[2], coefficients->values[3]);
+        all_planes_.push_back( pm ) ;
+
+        for(size_t i=0; i<inliers->indices.size(); i++)
+            filter_mask_[ inliers->indices[i] ] = false;
+    }
+}
+
+template <typename PointT>
+void
+EuclideanSegmenter<PointT>::segment()
+{
+    clusters_.clear();
+    filter_mask_.clear();
+    filter_mask_.resize(scene_->points.size(), true);
+
+    scene_xyz_.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    cloud_filtered.reset (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::copyPointCloud(*scene_, *scene_xyz_);
+    computeTablePlanes();
+
+    // remove nan points
+    for(size_t i=0; i<scene_xyz_->points.size(); i++)
+    {
+        if (!filter_mask_[i])
+            continue;
+
+        if( !pcl::isFinite( scene_xyz_->points[i] ) || scene_xyz_->points[i].z > param_.chop_z_ )
+            filter_mask_[i] = false;
+    }
+
+    pcl::copyPointCloud(*scene_xyz_, filter_mask_, *cloud_filtered);
+
+
+    std::vector<int> indices2originalMap (scene_xyz_->points.size());    // maps points from filtered point cloud to original cloud
+    size_t kept=0;
+    for(size_t i=0; i<filter_mask_.size(); i++)
+    {
+        if( filter_mask_[i] )
+            indices2originalMap[kept++] = i;
+    }
+    indices2originalMap.resize(kept);
+
+    typename pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud_filtered);
+
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (param_.cluster_tolerance_);
+    ec.setMinClusterSize (param_.min_cluster_size_);
+    ec.setMaxClusterSize (param_.max_cluster_size_);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_filtered);
+    ec.extract (clusters_);
+
+    // transform to original indices
+    for(size_t i=0; i < clusters_.size(); i++)
+    {
+        pcl::PointIndices &cluster = clusters_[i];
+        for(size_t pt_id=0; pt_id<cluster.indices.size(); pt_id++)
+        {
+            cluster.indices[pt_id] = indices2originalMap [ cluster.indices[pt_id] ];
+        }
+    }
+
+    if(visualize_)
+        this->visualize();
+}
+
 template class V4R_EXPORTS EuclideanSegmenter<pcl::PointXYZRGB>;
 }
