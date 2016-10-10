03dbf3e7adf91a610f1e90de6372705568dc2239
Georg Halmetschlager-Funek <georg.halmetschlager@gmail.com>
2016-10-10 10:04:52 +0200

    Added Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Installing Dependencies for V4R (Ubuntu 14.04 Trusty)..."
-
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
@@ -9,11 +8,3 @@ sudo rosdep init
 
 rosdep update
 rosdep install --from-paths . -i -y -r --rosdistro indigo #maybe 'indigo' has to be changed for Ubuntu 16.04 to 'jade' or 'kinetic'
-
-echo "Building V4R..."
-cpu=`nproc`
-cpu=$(($cpu -1)) #extracting num. cpu_cores-1
-mkdir build
-cd build
-cmake ..
-make -j$cpu
