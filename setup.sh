3ad74789be869f2afe1017e68568f7160c7c878c
Markus Bajones <markus.bajones@gmail.com>
2016-10-17 15:23:39 +0200

    escape variables

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ fi
 echo "Installing Dependencies for V4R (Ubuntu ${ubuntu_version} ${ubuntu_version_name} using ROS ${ros_version})..."
 echo "If you want to change this you can pass in the codename of the Ubuntu release. Eg. $0 xenial for 16.04"
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu ${ubuntu_version_name} main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu '${ubuntu_version_name}' main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
 sudo apt-get install -qq -y python-rosdep build-essential cmake
