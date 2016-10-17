a505b09d51bf56d47882a0a2b41323f12c236752
Markus Bajones <markus.bajones@gmail.com>
2016-10-17 16:41:34 +0200

    pass 2 parameters to setup. ubuntu and ros codename

--- setup.sh
+++ setup.sh
@@ -3,19 +3,23 @@
 if [ $# -eq 0 ]
   then
     # No arguments supplied
-    ubuntu_version="14.04"
     ubuntu_version_name="trusty"
     ros_version="indigo"
 
-elif [ $1 = "xenial" ]
+elif [ $# -eq 2 ]
   then
-    ubuntu_version="16.04"
-    ubuntu_version_name="xenial"
-    ros_version="kinetic"
+    ubuntu_version_name=${1}
+    ros_version=${2}
+
+else
+  echo "ERROR: Wrong usage"
+  echo "Usage: $0 ubuntu_code_name ros_code_name eg. $0 xenial kinetic"
+  exit 1
+
 fi
 
-echo "Installing Dependencies for V4R (Ubuntu ${ubuntu_version} ${ubuntu_version_name} using ROS ${ros_version})..."
-echo "If you want to change this you can pass in the codename of the Ubuntu release. Eg. $0 xenial for 16.04"
+echo "Installing Dependencies for V4R (Ubuntu ${ubuntu_version_name} using ROS ${ros_version})..."
+echo "If you want to change this you can pass in the codename of the Ubuntu release. Eg. $0 xenial kinetic for 16.04 with ROS kinetic"
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu '${ubuntu_version_name}' main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
