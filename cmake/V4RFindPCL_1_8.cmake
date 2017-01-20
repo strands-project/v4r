d5cab5c5a6b0b4f059f120b1eae127a709122e67
Thomas Faeulhammer <faeulhammer@acin.tuwien.ac.at>
2017-01-20 17:27:29 +0100

    getting mad

--- cmake/V4RFindPCL_1_8.cmake
+++ cmake/V4RFindPCL_1_8.cmake
@@ -2,8 +2,7 @@
   if(NOT PCL_1_8_FOUND)
     v4r_clear_vars(PCL_1_8_LIBRARY PCL_1_8_LIBRARIES PCL_1_8_INCLUDE_DIRS)
     set(PCL_1_8_LIBRARY pcl_1_8)
-    set(PCL_LIBRARIES "${PCL_LIBRARIES} ${PCL_1_8_LIBRARY}")
     add_subdirectory("${V4R_SOURCE_DIR}/3rdparty/pcl_1_8")
-    set(PCL_INCLUDE_DIRS "${PCL_INCLUDE_DIRS} ${V4R_SOURCE_DIR}/3rdparty")
+    set(PCL_1_8_INCLUDE_DIRS "${V4R_SOURCE_DIR}/3rdparty/pcl_1_8")
   endif()
 
