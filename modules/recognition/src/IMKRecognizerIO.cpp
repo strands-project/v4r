6fdeea22bf4cadbbda5188695a6b9278bdf992b6
Johann Prankl <hannes@runion.acin.tuwien.ac.at>
2017-01-24 18:05:36 +0100

    MIT license header

--- modules/recognition/src/IMKRecognizerIO.cpp
+++ modules/recognition/src/IMKRecognizerIO.cpp
@@ -1,34 +1,26 @@
-/**
- * $Id$
- * 
- * Software License Agreement (GNU General Public License)
+/******************************************************************************
+ * Copyright (c) 2016 Johann Prankl
  *
- *  Copyright (C) 2015:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *    Johann Prankl, prankl@acin.tuwien.ac.at
- *    Aitor Aldoma, aldoma@acin.tuwien.ac.at
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *      Automation and Control Institute
- *      Vienna University of Technology
- *      Gusshausstraße 25-29
- *      1170 Vienn, Austria
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author Johann Prankl, Aitor Aldoma
- *
- */
+ ******************************************************************************/
+
 
 #include <v4r/recognition/IMKRecognizerIO.h>
 #include <boost/algorithm/string.hpp>
