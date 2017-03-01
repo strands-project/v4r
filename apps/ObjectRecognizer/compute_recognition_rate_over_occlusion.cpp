724830c1838ac95bc95b86ce7924bd53f0e13d5b
Thomas Faeulhammer <faeulhammer@acin.tuwien.ac.at>
2017-03-01 11:17:26 +0100

    make eval compatible to new annotation format

--- apps/ObjectRecognizer/compute_recognition_rate_over_occlusion.cpp
+++ apps/ObjectRecognizer/compute_recognition_rate_over_occlusion.cpp
@@ -1,36 +1,71 @@
-#include <v4r/io/eigen.h>
 #include <v4r/io/filesystem.h>
 
-#include <pcl/io/pcd_io.h>
-#include <pcl/visualization/pcl_visualizer.h>
-
+#include <Eigen/Eigen>
 #include <iostream>
 #include <sstream>
 #include <fstream>
 #include <algorithm>    // std::next_permutation, std::sort
 
 #include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
+namespace bf = boost::filesystem;
 
-// -m /media/Data/datasets/TUW/models/ -t /media/Data/datasets/TUW/validation_set/ -g /media/Data/datasets/TUW/annotations/ -r /home/thomas/recognition_results_eval/
+// -g /media/Data/datasets/TUW/annotations/ -r /home/thomas/recognition_results_eval/
 
 float rotation_error_threshold_deg;
 float translation_error_threshold_m;
 float occlusion_threshold;
 
-typedef pcl::PointXYZRGB PointT;
+struct Hypothesis
+{
+    Eigen::Matrix4f pose;
+    float occlusion;
+};
+
+std::map<std::string, std::vector<Hypothesis> > readHypothesesFromFile( const std::string & );
+
+std::map<std::string, std::vector<Hypothesis> >
+readHypothesesFromFile( const std::string &filename )
+{
+    std::map<std::string, std::vector<Hypothesis> > hypotheses;
+
+    std::ifstream anno_f ( filename.c_str() );
+    std::string line;
+    while (std::getline(anno_f, line))
+    {
+        std::istringstream iss(line);
+        std::string model_name, occlusion_tmp;
+
+        Hypothesis h;
+        iss >> model_name >> occlusion_tmp;
+        occlusion_tmp = occlusion_tmp.substr( 1, occlusion_tmp.length() - 3 );
+        h.occlusion = 1.f-std::stof( occlusion_tmp );
+
+        for(size_t i=0; i<16; i++)
+            iss >> h.pose(i / 4, i % 4);
+
+        auto pose_it = hypotheses.find( model_name );
+        if( pose_it != hypotheses.end() )
+            pose_it->second.push_back( h ) ;
+        else
+            hypotheses[model_name] = std::vector<Hypothesis>(1, h);
+    }
+
+    return hypotheses;
+}
 
 int
 main (int argc, char ** argv)
 {
     std::string out_dir = "/tmp/recognition_rates_over_occlusion/";
-    std::string test_dir, gt_dir, models_dir, or_dir;
+    std::string gt_dir, or_dir;
+    bool use_generated_hypotheses = false;
 
     rotation_error_threshold_deg = 30.f;
     translation_error_threshold_m = 0.05f;
-    bool do_validation=false;
 
     std::stringstream description;
     description << "Tool to compute object instance recognition rate." << std::endl <<
@@ -44,97 +79,79 @@ main (int argc, char ** argv)
     po::options_description desc(description.str());
     desc.add_options()
         ("help,h", "produce help message")
-        ("test_dir,t", po::value<std::string>(&test_dir)->required(), "Root directory with test scenes stored as point clouds (.pcd).")
-        ("models_dir,m", po::value<std::string>(&models_dir)->required(), "Root directory containing the model files (i.e. filenames 3D_model.pcd).")
         ("groundtruth_dir,g", po::value<std::string>(&gt_dir)->required(), "Root directory containing annotation files (i.e. 4x4 ground-truth pose of each object with filename viewId_ModelId_ModelInstanceCounter.txt")
         ("rec_results_dir,r", po::value<std::string>(&or_dir)->required(), "Root directory containing the recognition results (same format as annotation files).")
         ("out_dir,o", po::value<std::string>(&out_dir)->default_value(out_dir), "Output directory where recognition results will be stored")
         ("trans_thresh", po::value<float>(&translation_error_threshold_m)->default_value(translation_error_threshold_m), "Maximal allowed translational error in metres")
         ("rot_thresh", po::value<float>(&rotation_error_threshold_deg)->default_value(rotation_error_threshold_deg), "Maximal allowed rotational error in degrees (NOT IMPLEMENTED)")
-        ("do_validation", po::bool_switch(&do_validation), "if set, it interprets the given rec_results_dir as the directory containing different test runs (e.g. different parameters). For each folder, it will generate a result file. Inside each folder there is the same structure as used without this parameter.")
+        ("use_generated_hypotheses", po::bool_switch(&use_generated_hypotheses), "if true, computes recognition rate for all generated hypotheses instead of verified ones.")
     ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
-    if (vm.count("help"))
-    {
-        std::cout << desc << std::endl;
-        return false;
-    }
+    if (vm.count("help")) { std::cout << desc << std::endl; return false; }
     try  {  po::notify(vm); }
     catch( std::exception& e)  { std::cerr << "Error: " << e.what() << std::endl << std::endl << desc << std::endl; return false; }
-    v4r::io::createDirIfNotExist(out_dir);
 
-    std::vector < std::string > model_files = v4r::io::getFilesInDirectory (models_dir, ".*3D_model.pcd",  true);
-    // we are only interested in the name of the model, so remove remaing filename
-    for(size_t model_id=0; model_id<model_files.size(); model_id++)
-        boost::replace_last(model_files[model_id], "/3D_model.pcd", "");
+    bf::path out_path = out_dir;
+    out_path /= "results_occlusion.txt";
 
-
-    std::vector<std::string> validation_sets;
-    if(do_validation)
-        validation_sets = v4r::io::getFoldersInDirectory ( or_dir );
-    else
-        validation_sets.push_back("");
-
-    for(size_t val_id=0; val_id<validation_sets.size(); val_id++)
+    if( v4r::io::existsFile( out_path.string() ) )
     {
-        const std::string out_fn = out_dir + "/results_occlusion_" + validation_sets[val_id] + ".txt";
-        if(v4r::io::existsFile(out_fn))
-        {
-            std::cout << out_fn << " exists already. Skipping it!" << std::endl;
-            continue;
-        }
-
-        std::ofstream f(out_fn);
-        std::cout << "Writing results to " << out_fn << "..." << std::endl;
-
-        std::vector < std::string > test_sets = v4r::io::getFoldersInDirectory ( test_dir );
-        if(test_sets.empty())
-            test_sets.push_back("");
-
-        for(size_t set_id=0; set_id<test_sets.size(); set_id++)
-        {
-           const std::string test_set_dir = test_dir + "/" + test_sets[set_id];
-           const std::string rec_dir = or_dir + "/" + validation_sets[val_id] + "/" + test_sets[set_id];
-           const std::string anno_dir = gt_dir + "/" + test_sets[set_id];
-
-           std::vector < std::string > view_files = v4r::io::getFilesInDirectory (test_set_dir, ".*.pcd",  true);
-           for(size_t view_id=0; view_id<view_files.size(); view_id++)
-           {
-               boost::replace_last(view_files[view_id], ".pcd", "");
-               for(size_t model_id=0; model_id<model_files.size(); model_id++)
-               {
-                   std::string search_pattern = view_files[view_id] + "_" + model_files[model_id] + "_";
-                   std::string regex_search_pattern = ".*" + search_pattern + "*.*txt";
-                   std::vector<std::string> rec_files = v4r::io::getFilesInDirectory (rec_dir, regex_search_pattern,  true);
-                   std::vector<std::string> gt_files  = v4r::io::getFilesInDirectory (anno_dir, regex_search_pattern,  true);
-
-                   for(size_t gt_id=0; gt_id<gt_files.size(); gt_id++) {
-                       std::string occlusion_file = gt_files[gt_id];
-                       boost::replace_first(occlusion_file, view_files[view_id], view_files[view_id] + "_occlusion");
-                       occlusion_file = anno_dir + "/" + occlusion_file;
-                       float occ = 0.f;
-                       if (!v4r::io::readFloatFromFile(occlusion_file, occ))
-                           cerr << "Did not find occlusion file " << occlusion_file << std::endl;
-
-                       Eigen::Matrix4f gt_pose_tmp =v4r::io::readMatrixFromFile(anno_dir + "/" + gt_files[gt_id]);
-
-                       bool is_recognized = false;
-                       for(size_t r_id=0; r_id<rec_files.size(); r_id++)
-                       {
-                           Eigen::Matrix4f rec_tmp = v4r::io::readMatrixFromFile(rec_dir + "/" + rec_files[r_id]);
-                           Eigen::Vector3f rec_trans = rec_tmp.block<3,1>(0,3);
-                           Eigen::Vector3f gt_trans = gt_pose_tmp.block<3,1>(0,3);
-
-                           if( (gt_trans-rec_trans).norm() < translation_error_threshold_m)
-                               is_recognized = true;
-                       }
-                       f << occ << " " << is_recognized << std::endl;
-                   }
-               }
-           }
-        }
-        f.close();
-        std::cout << "Done!" << std::endl;
+        std::cerr << out_path.string() << " exists already. Skipping it!" << std::endl;
+        return -1;
     }
+
+    v4r::io::createDirForFileIfNotExist( out_path.string() );
+
+    std::ofstream f( out_path.string() );
+    std::cout << "Writing results to " << out_path.string() << "..." << std::endl;
+
+    std::vector<std::string> annotation_files = v4r::io::getFilesInDirectory( gt_dir, ".*.anno", true );
+
+    for( const std::string anno_file : annotation_files )
+    {
+        bf::path gt_path = gt_dir;
+        gt_path /= anno_file;
+
+        std::string rec_file = anno_file;
+        if( use_generated_hypotheses )
+            boost::replace_last( rec_file, ".anno", ".generated_hyps");
+
+        bf::path rec_path = or_dir;
+        rec_path /= rec_file;
+
+        std::map<std::string, std::vector<Hypothesis> > gt_hyps = readHypothesesFromFile( gt_path.string() );
+        std::map<std::string, std::vector<Hypothesis> > rec_hyps = readHypothesesFromFile( rec_path.string() );
+
+        for(auto const &ent1 : gt_hyps)
+        {
+            for(auto const &h_gt : ent1.second)
+            {
+                bool is_recognized = false;
+
+                const Eigen::Matrix4f &gt_pose = h_gt.pose;
+                const Eigen::Vector3f gt_trans = gt_pose.block<3,1>(0,3);
+
+                float occlusion = h_gt.occlusion;
+
+                const auto it = rec_hyps.find( ent1.first );
+                if (it != rec_hyps.end())
+                {
+                    for(auto const &h_rec: it->second)
+                    {
+                        const Eigen::Matrix4f &rec_pose = h_rec.pose;
+                        const Eigen::Vector3f rec_trans = rec_pose.block<3,1>(0,3);
+
+
+                        if( (gt_trans-rec_trans).norm() < translation_error_threshold_m)
+                            is_recognized = true;
+                    }
+                }
+
+                f << occlusion << " " << is_recognized << std::endl;
+            }
+        }
+    }
+    f.close();
+    std::cout << "Done!" << std::endl;
 }
