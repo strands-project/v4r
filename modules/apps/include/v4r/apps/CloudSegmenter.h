e67b6b549ef942eed753458d7ecf330cbac52654
Thomas Faeulhammer <faeulhammer@acin.tuwien.ac.at>
2017-03-09 01:58:15 +0100

    add boost parameter

--- modules/apps/include/v4r/apps/CloudSegmenter.h
+++ modules/apps/include/v4r/apps/CloudSegmenter.h
@@ -38,7 +38,7 @@ class V4R_EXPORTS CloudSegmenterParameter
 public:
     float chop_z_; ///< cut-off distance in meter. Points further away than this threshold will be neglected
     float plane_inlier_threshold_;
-    size_t min_plane_inliers_;
+    size_t min_plane_inliers_; ///< minimum number of inlier points for a plane to be valid
     bool skip_segmentation_;    ///< if true, skips segmentation
     bool skip_plane_extraction_;    ///< if true, skips plane extraction
     bool only_remove_planes_;   ///< if true, removes plane from input cloud only. If false, removes plane and everything below it (i.e. further away from the camera)
@@ -55,6 +55,46 @@ public:
           only_remove_planes_(false),
           dominant_plane_only_(true)
     {}
+
+    /**
+     * @brief init parameters
+     * @param command_line_arguments (according to Boost program options library)
+     * @return unused parameters (given parameters that were not used in this initialization call)
+     */
+    std::vector<std::string>
+    init(int argc, char **argv)
+    {
+            std::vector<std::string> arguments(argv + 1, argv + argc);
+            return init(arguments);
+    }
+
+    /**
+     * @brief init parameters
+     * @param command_line_arguments (according to Boost program options library)
+     * @return unused parameters (given parameters that were not used in this initialization call)
+     */
+    std::vector<std::string>
+    init(const std::vector<std::string> &command_line_arguments)
+    {
+        po::options_description desc("Cloud Segmentation Parameter\n=====================\n");
+        desc.add_options()
+                ("help,h", "produce help message")
+                ("plane_inlier_threshold", po::value<float>(&plane_inlier_threshold_)->default_value(plane_inlier_threshold_), "inlier threshold for plane")
+                ("chop_z,z", po::value<float>(&chop_z_)->default_value(chop_z_), "cut-off threshold in meter")
+                ("min_plane_inliers", po::value<size_t>(&min_plane_inliers_)->default_value(min_plane_inliers_), "minimum number of inlier points for a plane to be valid")
+                ("skip_segmentation", po::value<bool>(&skip_segmentation_)->default_value(skip_segmentation_), " if true, skips segmentation")
+                ("only_remove_planes", po::value<bool>(&only_remove_planes_)->default_value(only_remove_planes_), "if true, removes plane from input cloud only. If false, removes plane and everything below it (i.e. further away from the camera)")
+                ("dominant_plane_only", po::value<bool>(&dominant_plane_only_)->default_value(dominant_plane_only_), "if true, removes only the plane with the largest number of plane inliers")
+                ;
+        po::variables_map vm;
+        po::parsed_options parsed = po::command_line_parser(command_line_arguments).options(desc).allow_unregistered().run();
+        std::vector<std::string> to_pass_further = po::collect_unrecognized(parsed.options, po::include_positional);
+        po::store(parsed, vm);
+        if (vm.count("help")) { std::cout << desc << std::endl; to_pass_further.push_back("-h"); }
+        try { po::notify(vm); }
+        catch(std::exception& e) {  std::cerr << "Error: " << e.what() << std::endl << std::endl << desc << std::endl; }
+        return to_pass_further;
+    }
 };
 
 /**
