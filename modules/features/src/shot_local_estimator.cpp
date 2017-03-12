35255eef9a101de4144f45e9d2f12ab6dd201f28
Thomas Faeulhammer <faeulhammer@acin.tuwien.ac.at>
2017-03-12 23:36:15 +0100

    remove nan points before  shot computation

--- modules/features/src/shot_local_estimator.cpp
+++ modules/features/src/shot_local_estimator.cpp
@@ -36,17 +36,50 @@ SHOTLocalEstimation<PointT>::compute (std::vector<std::vector<float> > & signatu
 //        computeNormals<PointT>(processed_, processed_normals, param_.normal_computation_method_);
 //    }
 
+    CHECK( cloud_->points.size() == normals_->points.size() );
+
+    typename pcl::PointCloud<PointT>::Ptr cloud_wo_nan (new pcl::PointCloud<PointT>);
+    pcl::PointCloud<pcl::Normal>::Ptr normals_wo_nan (new pcl::PointCloud<pcl::Normal>);
+    cloud_wo_nan->points.resize( cloud_->points.size() );
+    normals_wo_nan->points.resize( cloud_->points.size() );
+    std::vector<int> originalIndices2new ( cloud_->points.size(), -1 );
+
+    size_t kept = 0;
+    for(size_t i=0; i<cloud_->points.size(); i++)
+    {
+        const PointT &p = cloud_->points[i];
+        const pcl::Normal &n = normals_->points[i];
+        if( pcl::isFinite(p) && pcl::isFinite(n) )
+        {
+            cloud_wo_nan->points[kept] = p;
+            normals_wo_nan->points[kept] = n;
+            originalIndices2new[i] = kept;
+            kept++;
+        }
+    }
+    cloud_wo_nan->points.resize(kept);
+    cloud_wo_nan->width = kept;
+    cloud_wo_nan->height = 1;
+    normals_wo_nan->points.resize(kept);
+    normals_wo_nan->width = kept;
+    normals_wo_nan->height = 1;
+    originalIndices2new.resize(kept);
+
+
     typename pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+    tree->setInputCloud(cloud_wo_nan);
     pcl::PointCloud<pcl::SHOT352> shots;
     pcl::SHOTEstimationOMP<PointT, pcl::Normal, pcl::SHOT352> shot_estimate;
     shot_estimate.setNumberOfThreads (0);
     shot_estimate.setSearchMethod (tree);
-    shot_estimate.setInputCloud (cloud_);
-    shot_estimate.setInputNormals (normals_);
-    boost::shared_ptr<std::vector<int> > IndicesPtr (new std::vector<int>);
-    *IndicesPtr = indices_;
+    shot_estimate.setInputCloud (cloud_wo_nan);
+    shot_estimate.setInputNormals (normals_wo_nan);
+    boost::shared_ptr<std::vector<int> > IndicesPtr (new std::vector<int> (indices_.size() ));
+    for(size_t i=0; i<indices_.size(); i++)
+        IndicesPtr->at(i) = originalIndices2new[ indices_[i] ];
+
     shot_estimate.setIndices(IndicesPtr);
-    shot_estimate.setRadiusSearch (support_radius_);
+    shot_estimate.setRadiusSearch (param_.support_radius_);
     shot_estimate.compute (shots);
 
     CHECK( shots.points.size() == indices_.size() );
