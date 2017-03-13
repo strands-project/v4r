a168cc86c23b705fcacc7f68ddc757a2b185d7aa
Thomas Faeulhammer <faeulhammer@acin.tuwien.ac.at>
2017-03-13 13:09:13 +0100

    visualize normals in processed cloud

--- modules/apps/src/visualization.cpp
+++ modules/apps/src/visualization.cpp
@@ -183,7 +183,7 @@ ObjectRecognitionVisualizer<PointT>::visualize() const
     vis_cloud->sensor_orientation_ = Eigen::Quaternionf::Identity();
 
     if(normals_)
-        vis_->addPointCloudNormals<PointT,pcl::Normal>( cloud_, normals_, 100, 0.02f, "normals", vp1a_);
+        vis_->addPointCloudNormals<PointT,pcl::Normal>( processed_cloud_, normals_, 50, 0.02f, "normals", vp1b_);
 
 #if PCL_VERSION >= 100800
     vis_->removeAllCoordinateSystems(vp2_);
