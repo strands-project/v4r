8e36344b83da5a766e0cfc950d6b236a4020fd33
Thomas Faeulhammer <faeulhammer@acin.tuwien.ac.at>
2017-04-21 13:08:12 +0200

    use auto

--- modules/common/src/normal_estimator_integral_image.cpp
+++ modules/common/src/normal_estimator_integral_image.cpp
@@ -29,13 +29,11 @@ NormalEstimatorIntegralImage<PointT>::compute()
 
     for(pcl::Normal &n : normal_->points)
     {
-        Eigen::Vector3f normal = n.getNormalVector3fMap();
+        auto normal = n.getNormalVector3fMap();
         if ( normal.dot(Eigen::Vector3f::UnitZ()) > 0 ) //flip normal towards viewpoint
             normal = -normal;
 
         normal.normalize();
-
-        n.getNormalVector3fMap() = normal;
     }
 
     return normal_;
